1f99c8a5a348e31a296ad6597ef0ad32486a1933
ust154 <ustas1987@gmail.com>
2019-06-20 17:55:06 +0300

    new version of class UserShips

--- src/com/ustas1987/game1/Main.java
+++ src/com/ustas1987/game1/Main.java
@@ -2,10 +2,18 @@ package com.ustas1987.game1;
 
 public class Main {
     public static void main(String[] args) {
+        //UserShips userShips = new UserShips();
+
         Field.creatTheField();
         Field.showTheField();
 
-        UserShips.createUserShips();
-        UserShips.setTheShipsOnTheField();
+        //UserShips.createUserShips();
+        //UserShips.setTheShipsOnTheField();
+       // UserShips.autoSetUserShipsOnTheField(1,10);
+        //UserShips.autoSetUserShipsOnTheField();
+    UserShips.createUserShips();
+    UserShips.autoSetUserShipsOnTheField();
+
     }
+
 }
